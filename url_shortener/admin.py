d2cd664a1cb6bbfa2695fc4ffbf2652b9262d3c3
Artem Kupriyanov <kupriyanovart@gmail.com>
2021-08-14 16:24:05 +0500

    Создал модель, сделал миграции, добавил модель в админ панель

--- url_shortener/admin.py
+++ url_shortener/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Urls
+
+
+class UrlsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Urls, UrlsAdmin)
+
--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,3 +1,15 @@
+import hashlib
+
 from django.db import models
 
-# Create your models here.
+
+class Urls(models.Model):
+    class Meta:
+        verbose_name = "Url"
+        verbose_name_plural = "Urls"
+    url = models.URLField(unique=True)
+    hash_url = models.URLField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.hash_url = hashlib.sha256(self.url.encode()).hexdigest()[:10]
+        return super().save(*args, **kwargs)
